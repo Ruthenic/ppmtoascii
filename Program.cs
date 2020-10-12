9ab9088f7305280638a8e6c5c5c00fbb6c96e9fd
Ruthenic <mdrakea3@tutanota.com>
2020-10-12 11:17:00 -0400

    add nonfunctional (currently) output to text code

--- Program.cs
+++ Program.cs
@@ -15,22 +15,35 @@ namespace ppmtoascii
             string magic;
             string heightwidth;
             string maxcol;
+            File.WriteAllText("output.txt", "");
+            StreamWriter sr = new StreamWriter("output.txt");
             using (StreamReader streamread = new StreamReader(ppmpath)){
                 magic = streamread.ReadLine();
                 heightwidth = streamread.ReadLine();
                 maxcol = streamread.ReadLine();
             }
             string[] hw = heightwidth.Split(null);
-            string height = hw[0];
-            string width = hw[1];
+            string width = hw[0];
+            string height = hw[1];
             List<byte> pixelinfo;
             List<string> lines = ((string[])File.ReadAllLines(ppmpath)).ToList();
             Console.WriteLine(magic + ", " + heightwidth + ", " + maxcol);
             lines.RemoveRange(0, 3);
             File.WriteAllLines("unheaderedppm.binary", lines.ToArray());
             pixelinfo = File.ReadAllBytes("unheaderedppm.binary").ToList();
+            int i = 0;
             foreach (byte bytey in pixelinfo.ToArray()) {
                 Console.WriteLine(bytey);
+                if (i <= int.Parse(width)) {
+                    sr.WriteLine("");
+                }
+                if (bytey >= 1) {
+                    sr.Write("$");
+                    i+= 1;
+                } else {
+                    sr.Write(" ");
+                    i+= 1;
+                }
             }
             File.Delete("unheaderedppm.binary");
         }
